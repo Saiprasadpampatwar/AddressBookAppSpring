9290267ae7a8747af9129f9145f0dfbddfede00c
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-20 21:39:24 +0530

    [Saiprasad]Added: ability to validate fields in addressbookdata and through user friendly responces

--- src/main/java/com/saischool/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/saischool/addressbookapp/dto/AddressBookDTO.java
@@ -3,13 +3,13 @@ package com.saischool.addressbookapp.dto;
 import javax.validation.constraints.Pattern;
 
 public class AddressBookDTO {
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Person Name Invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Person firstName Invalid")
     public String firstName;
 
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Person Name Invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Person lastName Invalid")
     public String lastName;
 
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Person Name Invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Person Address Invalid")
     public String address;
 
     public AddressBookDTO(String firstName, String lastName, String address) {
