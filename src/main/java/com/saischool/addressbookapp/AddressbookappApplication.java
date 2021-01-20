bc80c5df559fde5b349f60e7441608452b98d59f
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-20 21:04:18 +0530

    [Saiprasad]Added: Lambok for logging

--- src/main/java/com/saischool/addressbookapp/AddressbookappApplication.java
+++ src/main/java/com/saischool/addressbookapp/AddressbookappApplication.java
@@ -1,13 +1,17 @@
 package com.saischool.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AddressbookappApplication.class, args);
+        log.info("AddressBook App Started");
+
     }
 
 }
